b9e3ef05c38763f5ea7ce85219d30d2667ec5743
whoisthatme <Ychinin@yandex.ru>
2024-03-25 16:53:22 +0300

    1

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -10,44 +10,39 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class MainApp {
-   public static void main(String[] args) throws SQLException {
-      AnnotationConfigApplicationContext context = 
-            new AnnotationConfigApplicationContext(AppConfig.class);
+    public static void main(String[] args) throws SQLException {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
 
-      UserService userService = context.getBean(UserService.class);
+        UserService userService = context.getBean(UserService.class);
 
-      User user1 = new User("username1", "lastname1", "email1");
-      User user2 = new User("username2", "lastname2", "email2");
-      User user3 = new User("username3", "lastname3", "email3");
-      User user4 = new User("username4", "lastname4", "email4");
+        User user1 = new User("username1", "lastname1", "email1");
+        User user2 = new User("username2", "lastname2", "email2");
+        User user3 = new User("username3", "lastname3", "email3");
+        User user4 = new User("username4", "lastname4", "email4");
 
-      Car car1 = new Car("honda", 1998);
-      Car car2 = new Car("reno", 1999);
-      Car car3 = new Car("bmw", 2000);
-      Car car4 = new Car("citroen", 2001);
+        Car car1 = new Car("honda", 1998);
+        Car car2 = new Car("reno", 1999);
+        Car car3 = new Car("bmw", 2000);
+        Car car4 = new Car("citroen", 2001);
 
-      userService.add(user1.setCar(car1).setUser(user1));
-      userService.add(user2.setCar(car2).setUser(user2));
-      userService.add(user3.setCar(car3).setUser(user3));
-      userService.add(user4.setCar(car4).setUser(user4));
-
-
+        userService.add(user1.setCar(car1).setUser(user1));
+        userService.add(user2.setCar(car2).setUser(user2));
+        userService.add(user3.setCar(car3).setUser(user3));
+        userService.add(user4.setCar(car4).setUser(user4));
 
 
+        List<User> users = userService.listUsers();
+        for (User user : users) {
+            System.out.println("Id = " + user.getId());
+            System.out.println("First Name = " + user.getFirstName());
+            System.out.println("Last Name = " + user.getLastName());
+            System.out.println("Email = " + user.getEmail());
+            System.out.println();
+        }
 
+        System.out.println(userService.getUserByCar("citroen", 2001));
 
-
-      List<User> users = userService.listUsers();
-      for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println();
-      }
-
-      System.out.println(userService.getUserByCar("citroen", 2001));
-
-      context.close();
-   }
+        context.close();
+    }
 }
--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -6,77 +6,78 @@ import javax.persistence.*;
 @Table(name = "users")
 public class User {
 
-   @Id
-   @GeneratedValue(strategy = GenerationType.IDENTITY)
-   private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-   @Column(name = "name")
-   private String firstName;
+    @Column(name = "name")
+    private String firstName;
 
-   @Column(name = "last_name")
-   private String lastName;
+    @Column(name = "last_name")
+    private String lastName;
 
-   @Column(name = "email")
-   private String email;
-   @OneToOne(cascade = CascadeType.ALL,mappedBy = "user")
-   private Car car;
+    @Column(name = "email")
+    private String email;
+    @OneToOne(cascade = CascadeType.ALL, mappedBy = "user")
+    private Car car;
 
-   public User() {}
+    public User() {
+    }
 
-   public Car getCar() {
-      return car;
-   }
+    public Car getCar() {
+        return car;
+    }
 
-   public Car setCar(Car car) {
-       this.car = car;
-       return car;
-   }
+    public Car setCar(Car car) {
+        this.car = car;
+        return car;
+    }
 
-   public User(String firstName, String lastName, String email) {
-      this.firstName = firstName;
-      this.lastName = lastName;
-      this.email = email;
-   }
+    public User(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
 
-   public Long getId() {
-      return id;
-   }
+    public Long getId() {
+        return id;
+    }
 
-   public void setId(Long id) {
-      this.id = id;
-   }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-   public String getFirstName() {
-      return firstName;
-   }
+    public String getFirstName() {
+        return firstName;
+    }
 
-   public void setFirstName(String firstName) {
-      this.firstName = firstName;
-   }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-   public String getLastName() {
-      return lastName;
-   }
+    public String getLastName() {
+        return lastName;
+    }
 
-   public void setLastName(String lastName) {
-      this.lastName = lastName;
-   }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-   public String getEmail() {
-      return email;
-   }
+    public String getEmail() {
+        return email;
+    }
 
-   public void setEmail(String email) {
-      this.email = email;
-   }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-   @Override
-   public String toString() {
-      return "User{" +
-              "id=" + id +
-              ", firstName='" + firstName + '\'' +
-              ", lastName='" + lastName + '\'' +
-              ", email='" + email + '\'' +
-              '}';
-   }
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
